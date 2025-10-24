6021d356986938a312dc20c64d2e8c794248fe8f
KYS <ehcl1027@gmail.com>
2025-10-24 17:38:37 +0900

    feat: 자동차 이름 검증 로직 구현

---
+++ src/main/java/racingcar/validator/NameInputValidator.java
@@ -0,0 +1,23 @@
+package racingcar.validator;
+
+public class NameInputValidator {
+    private NameInputValidator() {
+    }
+
+    public static void validateNames(String validInput) {
+        String[] names = validInput.split(",");
+        for (String name : names) {
+            validateSingleName(name);
+        }
+    }
+
+    private static void validateSingleName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
