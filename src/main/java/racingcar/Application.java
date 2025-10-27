0c853fc7cf796f8d8771476a6f2b5a8706382274
KYS <ehcl1027@gmail.com>
2025-10-27 16:52:07 +0900

    feat: Application 구현 및 의존성 주입
    - main 메서드 내에서 애플리케이션 실행에 필요한 모든 객체를 생성합니다.
    - 생성자 주입(DI)을 통해 각 객체의 의존성을 설정 후에, Controller를 중심으로 전체 컴포넌트를 조립합니다.
    - racingController.run()을 호출하여 실제 애플리케이션 로직을 실행합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.util.NumberGeneratorUtil;
+import racingcar.util.RandomNumberGeneratorUtil;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        NumberGeneratorUtil numberGeneratorUtil = new RandomNumberGeneratorUtil();
+
+        final RacingController racingController = new RacingController(
+                inputView,
+                outputView,
+                numberGeneratorUtil
+        );
+
+        racingController.run();
     }
 }
